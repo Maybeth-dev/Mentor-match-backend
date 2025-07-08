76018ca293dffefe9e0083539b388342f3475af0
martha <marthaobasa@gmail.com>
2025-07-08 08:18:23 +0100

    fixed Url

--- src/app.ts
+++ src/app.ts
@@ -12,10 +12,12 @@ import adminRoutes from './routes/admin';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000 ;
 
-
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+}));
 app.use(express.json());
 
 
